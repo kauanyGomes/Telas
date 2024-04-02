6666256935f94a5dadce2e4fe3ed4cf778f11c95
kauany <kauany.gomes@estudante.ifms.edu.br>
2024-04-02 11:18:35 -0400

    02/04

--- App.js
+++ App.js
@@ -17,32 +17,51 @@
 // export default App; //poderia por antes da function
 
 
+//usando classe
 import React, {Component} from "react";
-import {View, Text, Image} from "react-native";
+import {View, Text, Image} from "react-native"; //importando agora a imagem
+
+class App extends Component{ //extendes = herança dos componetes, tudo que um tem passa pro outro
+  render() { //renderizar = mostrar na tela, chamando a função
+    let curso = "INFO";
 
 
-class App extends Component{
-  render() {
+    return(
+      <View> {/* componete */}
+        <Text>Olá mundo!</Text>
+        <Text>Exemplo!</Text>
+        <Text style = {{color: 'blue', fontSize: 30, margin: 15}}>Meu exemplo!</Text> 
 
-let curso = "INFO";
+        <Image source = {{uri: 'coloca a URL aqui, https'}} style = {{width: 300, height: 300}}/>
+        {/* Image é obrigado a informar a largura e a altura */}
+        <Text>{curso}</Text> {/*usar variável colocando entre chaves*/ }
 
+        {/* para chamar o componente*/}
+        {/* <MinhaImagem> */}
+        {/* passando as prpriedades a largura e a altura */}
+        <MinhaImagem largura={400} altura={400} nome="Imagem 1 "/> {/*propriedades sendo chamadas na outra classe, chamando */}
+      </View>
 
-return(
-  <View>
-    <Text>Olá mundo!</Text>
-    <Text>Exemplo!</Text>
-    <Text style = {{color: 'blue', fontSize: 30, margin: 15}}>Meu exemplo!</Text>
-
-
-    <Image source = {{uri: 'coloca a URL aqui'}} style = {{width: 300, height: 300}}/>
-      {/* Image é obrigado a informar a largura e a altura */}
-    <Text>{curso}</Text>
-
-    {/* para chamar o componente*/}
-    {/*  */}
-  </View>
-)
+    );
 
   }
 }
 
+export default App;
+
+class MinhaImagem extends Component{
+  render() {
+
+    let img = "url da imagem "; /*colocando url na variável, para depois chaamala */
+    return(
+      <View>
+        {/* <Image source={{uri: img}} style={{width: 300, height: 300}}/> */}
+
+        <Image source={{uri: img}} style={{width: this.props.largura, height:this.props.altura}}/> {/*pegando as propriedades de outra classe */}
+
+          <Text>{this.props.nome}</Text> {/*dessa propriedade vou chamar o nome */}
+
+      </View>
+    );
+  }
+}
